064ccdd587515ae3b52ca8d964426c070ee94051
KD2-Sushil-83493 <sushilsarap17@gmail.com>
2025-04-04 17:09:05 +0530

    create and test API Route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { error } from 'console';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{console.log("Connected to Mongo DB!")})
@@ -12,3 +13,5 @@ const app= express();
 app.listen(3000, ()=>{
     console.log('Server Started on port 3000');
 });
+
+app.use('/api/user', userRouter);
