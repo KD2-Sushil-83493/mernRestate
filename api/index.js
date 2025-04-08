b82e38a6e504e1db3b2a7863fb715b0658bab79b
KD2-Sushil-83493 <sushilsarap17@gmail.com>
2025-04-08 18:07:51 +0530

    create sign up API route

--- api/index.js
+++ api/index.js
@@ -3,15 +3,18 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { error } from 'console';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{console.log("Connected to Mongo DB!")})
 .catch((err)=>{console.log(err)});
 
 const app= express();
+app.use(express.json());
 
 app.listen(3000, ()=>{
     console.log('Server Started on port 3000');
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
