e2f5e47c82fd74c6fa2428ba6383784f7090fd4c
KD2-Sushil-83493 <sushilsarap17@gmail.com>
2025-04-20 12:56:12 +0530

    create sign-up page UI

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function SignIn() {
   return (
-    <div>SignIn</div>
+    <div className='p-4 max-w-sm mx-auto'>
+      <h1 className='text-slate-900 font-bold text-center my-8'>Login</h1>
+      <form className='flex flex-col gap-4'>
+        <input type='text' placeholder='Username' className='border rounded-xl p-2' />
+        <input type='password' placeholder='Password' className='border rounded-xl p-2' />
+
+      </form>
+      <div className='p-2'>Dont have an account? <a href='http://localhost:5173/sign-up' className='text-blue-700 font-semibold '>Sign-Up</a></div>
+    </div>
   )
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-slate-950 font-bold text-center my-8'>SignUp</h1>
+      <form className='flex flex-col gap-4'>
+        <input type='text' placeholder='Username'
+        className='border p-2 rounded-xl' id='username' />
+        <input type='email' placeholder='Email'
+        className='border p-2 rounded-xl' id="email" />
+        <input type='password' placeholder='Password'
+        className='border p-2 rounded-xl' id="password" />
+      </form>
+
+      <div className='p-2'>
+        <h1>Already an account? <a href='http://localhost:5173/sign-in' className='text-blue-700 font-semibold'>Sign-In</a></h1>
+      </div>
+    </div>
   )
 }
